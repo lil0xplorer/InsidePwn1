dde85abe628e3b78303c71fc8aa8cabca8346627
Krrish Bhurani <90558815+lil0xplorer@users.noreply.github.com>
2025-05-20 14:32:11 +0530

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -10,12 +10,7 @@ const CONFIG = {
     github: "",
     instagram: "",
   },
-  projects: [
-    {
-      name: `morethan-log`,
-      href: "https://github.com/morethanmin/morethan-log",
-    },
-  ],
+  
   // blog setting (required)
   blog: {
     title: "morethan-log",
